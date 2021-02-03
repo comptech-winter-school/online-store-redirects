07eb1849fcc14cac1c4640957294e9ab1227b14c
studokim <studokim@gmail.com>
2021-02-03 14:02:37 +0300

    the whole node branch is now ignored

--- utils/cousins.py
+++ utils/cousins.py
@@ -1,21 +1,16 @@
 from random import randint
 
-def cousin(node):
-    ancestor = node.parent
-    while len(ancestor.children) < 2:
-        ancestor = ancestor.parent
-    n = len(ancestor.children)
-    while True:
-        child = ancestor.children[randint(0, n - 1)]
-        if child != node:
-            break
-    return child
+def get_cousin(node):
+    cousins = get_cousins(node)
+    n = len(cousins)
+    return cousins[randint(0, n - 1)]
 
-def cousins(node):
+def get_cousins(node):
     ancestor = node.parent
+    node_branch = node
     while len(ancestor.children) < 2:
+        node_branch = ancestor
         ancestor = ancestor.parent
     cousins = list(ancestor.children)
-    if (node in cousins):
-        cousins.remove(node)
+    cousins.remove(node_branch)
     return cousins
