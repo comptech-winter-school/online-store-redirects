3d5b98ef4b8cc9a275334af24802911d1ea2dd0f
studokim <studokim@gmail.com>
2021-02-04 14:21:13 +0300

    naming fixed

--- utils/delete_category.py
+++ utils/delete_category.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-def delete_orphan_categories_from_csv(dataFrame):
+def delete_orphan_categories(dataFrame):
     return dataFrame.query('(parent_id.isnull()) or (parent_id in id)')
 
-def delete_category(id_to_delete):
+def delete_category_from_csv(id_to_delete):
     categories = pd.read_csv('../resourses/category_tree/categories.csv')
     categories = categories.query('id != {0}'.format(id_to_delete))
-    categories = delete_orphan_categories_from_csv(categories)
+    categories = delete_orphan_categories(categories)
     categories.to_csv('../resourses/category_tree/categories.csv', index=False)
