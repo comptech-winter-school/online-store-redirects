0e6fc8cc6977ace0fb2c960416c24d97a7516ffd
studokim <studokim@gmail.com>
2021-02-04 14:28:33 +0300

    path fixed

--- utils/delete_category.py
+++ utils/delete_category.py
@@ -4,7 +4,7 @@ def delete_orphan_categories(dataFrame):
     return dataFrame.query('(parent_id.isnull()) or (parent_id in id)')
 
 def delete_category_from_csv(id_to_delete):
-    categories = pd.read_csv('../resourses/category_tree/categories.csv')
+    categories = pd.read_csv('resourses/category_tree/categories.csv')
     categories = categories.query('id != {0}'.format(id_to_delete))
     categories = delete_orphan_categories(categories)
-    categories.to_csv('../resourses/category_tree/categories.csv', index=False)
+    categories.to_csv('resourses/category_tree/categories.csv', index=False)
