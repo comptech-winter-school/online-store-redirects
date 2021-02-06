504fb81434686486319b9c842b96a4cbef479028
studokim <studokim@gmail.com>
2021-02-06 15:49:43 +0300

    get_category_tree() comments updated

--- utils/category_tree.py
+++ utils/category_tree.py
@@ -34,9 +34,8 @@ from utils.io_custom import read_pickle_object
 
 def get_category_tree(path_to_data):
     """
-    :param path: path to file with category tree
-    :return: dict(tree_dict) with int keys and values - anytree.Nodes
-    (tree_dict[-1] contains root of category tree)
+    :param path: path to file with binary category tree
+    :return: root node of type anytree.Node, it contains all the tree data
     """
     return read_pickle_object(path_to_data + '/category_tree.obj')
 
